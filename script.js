fa6f390580f67886f6272f4531ab266dc3708f79
DaleAllen28 <daleallendeveloper@gmail.com>
2022-04-10 23:14:26 +0100

    projects and project selector functionality

--- script.js
+++ script.js
@@ -30,31 +30,91 @@ function showImage(imgs) {
     };*/
 }
 
-/*OBJECT FOR PORTFOLIO PROJECTS*/
+/*OBJECT FOR PORTFOLIO PROJECT INFORMATION*/
 const projects = {
     tasklist: {
+        id: "tasklist",
         name: "To-do List",
         languages: ["HTML","JS","CSS"],
         links: {
             deploy: "https://cheery-crepe-6a2055.netlify.app",
             github: "https://github.com/DaleAllen28/To-do-List"
         },
-        description: "A basic to do list with date/time, completion, edit and delete functionality"
-    },
-    stopwatch: {
-        name: "Stopwatch",
-        languages: ["HTML","JS", "CSS"],
-        links: {
-            github: "https://github.com/DaleAllen28/stopwatch"
-        },
-        description: "A simple stopwatch with minute display and basic lap functionality."
+        description: "A basic to do list with date/time, completion, edit and delete functionality",
+        percentages: {
+            HTML: 9.5,
+            JS: 55.9,
+            CSS: 34.6
+        }
     },
     digitalclock: {
+        id: "digitalclock",
         name: "Digital Clock",
         languages: ["HTML", "JS", "CSS"],
         links: {
             github: "https://github.com/DaleAllen28/digitalclock"
         },
-    }
+        description: "Simple digital clock",
+        percentages: {
+            HTML: 18.1,
+            JS: 65.9,
+            CSS: 16.0
+        }
+    },
+    stopwatch: {
+        id: "stopwatch",
+        name: "Stopwatch",
+        languages: ["HTML","JS", "CSS"],
+        links: {
+            github: "https://github.com/DaleAllen28/stopwatch"
+        },
+        description: "A simple stopwatch with minute display and basic lap functionality.",
+        percentages: {
+            HTML: 15.5,
+            JS: 67.1,
+            CSS: 17.4
+        }
+    } 
 };
 
+/*FUNCTION TO DISPLAY PROJECT AND EDIT TEXT. !NOT WORKING! Try getAttribute for id*/
+const displayProject = (el) => {
+    let dispBox = document.getElementById("displayBox");
+    let projTitle = document.getElementById("pro_title");
+    let projDescription = document.getElementById("pro_desc");
+
+    let selectedProj = el.getAttribute("id");
+/*
+    if(selectedProj == projects.tasklist.id) {
+        dispBox.src = el.src;
+        projTitle.innerHTML = projects.tasklist.name;
+        projDescription.innerHTML = projects.tasklist.description;
+    } else if (selectedProj == projects.digitalclock.id) {
+        dispBox.src = el.src;
+        projTitle.innerHTML = projects.digitalclock.name;
+        projDescription.innerHTML = projects.digitalclock.description;
+    } else if (selectedProj == projects.stopwatch.id){
+        dispBox.src = el.src;
+        projTitle.innerHTML = projects.stopwatch.name;
+        projDescription.innerHTML = projects.stopwatch.description;
+    } */
+
+    switch (selectedProj)
+    {
+        case "tasklist":
+            dispBox.src = el.src;
+            projTitle.innerHTML = projects.tasklist.name;
+            projDescription.innerHTML = projects.tasklist.description;
+            break;
+        case "digitalclock":
+            dispBox.src = el.src;
+            projTitle.innerHTML = projects.digitalclock.name;
+            projDescription.innerHTML = projects.digitalclock.description;
+            break;
+        case "stopwatch":
+            dispBox.src = el.src;
+            projTitle.innerHTML = projects.stopwatch.name;
+            projDescription.innerHTML = projects.stopwatch.description;
+            break;
+    }
+};
